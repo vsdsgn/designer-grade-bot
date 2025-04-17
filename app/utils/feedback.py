f5a47978207f6f6bf397a617eb263bdc8514f112
Stephane Vasadze <vasdesign16@mail.ru>
2025-04-17 22:11:58 +0300

    “Обнова”

--- app/utils/feedback.py
+++ app/utils/feedback.py
@@ -4,6 +4,6 @@ import os
 FEEDBACK_PATH = "data_storage/feedback.txt"
 os.makedirs(os.path.dirname(FEEDBACK_PATH), exist_ok=True)
 
-def handle_feedback(chat_id, text):
+def save_feedback(chat_id, text):
     with open(FEEDBACK_PATH, "a", encoding="utf-8") as f:
         f.write(f"From {chat_id}: {text}\n{'-'*40}\n")
